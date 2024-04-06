02b523f5ff1284c5bf61300942b58d02f38fa9fb
Matheus Costa <bradventurer@gmail.com>
2024-04-06 12:30:19 -0300

    Criar análise de formas de pagamento mais utilizadas x div no cartao de credito

---
+++ pages/formas_de_pagamento_mais_utilizadas.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+def analyze_data(df):
+    st.title("Análise de Formas de Pagamento e Parcelamento")
+
+    payment_counts = df['payment_type'].value_counts()
+    st.subheader("Formas de Pagamento Mais Utilizadas:")
+    st.write(payment_counts)
+
+    avg_installments = df[df['payment_type'] == 'credit_card']['payment_installments'].mean()
+    st.subheader("Média de Parcelas para Compras com Cartão de Crédito:")
+    st.write(round(avg_installments, 2))
+
+file_path = './data/olist_order_payments_dataset.csv'
+data = load_data(file_path)
+
+analyze_data(data)
